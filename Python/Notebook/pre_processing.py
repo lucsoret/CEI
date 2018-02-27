2ccb01f62f7f6eb4720701cbf2e3e5d44c45f37f
Luc Soret <luc.soret@gmail.com>
2018-02-27 15:23:31 +0100

    MaJ_Luc

--- Python/Notebook/pre_processing.py
+++ Python/Notebook/pre_processing.py
@@ -16,8 +16,16 @@ from skimage.morphology import ball, disk, dilation, binary_erosion, remove_smal
 
 def load_scan(path):
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+        
     return slices
-
 
 
 def PixelData_to_HU(slices):
