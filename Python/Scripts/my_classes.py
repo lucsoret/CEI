becb8a57d496a8d218c3ad2ce662b08f52d022b8
Luc Soret <luc.soret@gmail.com>
2018-03-19 19:25:54 +0100

    script generation

---
+++ Python/Scripts/my_classes.py
@@ -0,0 +1,61 @@
+import numpy as np
+import os
+from pre_processing import full_process
+class DataGenerator(object):
+  'Generates data for Keras'
+  def __init__(self, dim_x = 450, dim_y = 450, dim_z = 400, batch_size = 1, shuffle = True):
+      'Initialization'
+      self.dim_x = dim_x
+      self.dim_y = dim_y
+      self.dim_z = dim_z
+      self.batch_size = batch_size
+      self.shuffle = shuffle
+#list_IDs = INPUT_FOLDER
+  def generate(self, labels, list_IDs):
+      'Generates batches of samples'
+      # Infinite loop
+      while 1:
+          # Generate order of exploration of dataset
+          indexes = self.__get_exploration_order(list_IDs)
+
+          # Generate batches
+          imax = int(len(indexes)/self.batch_size)
+          for i in range(imax):
+              # Find list of IDs
+              list_IDs_temp = [k for k in indexes[i*self.batch_size:(i+1)*self.batch_size]]
+
+              # Generate data
+              X, y = self.__data_generation(labels, list_IDs_temp,list_IDs)
+
+              yield X, y
+
+  def __get_exploration_order(self, list_IDs):
+      'Generates order of exploration'
+      # Find exploration order
+      patients = os.listdir(list_IDs)
+      indexes = np.arange(len(patients))
+      if self.shuffle == True:
+          np.random.shuffle(indexes)
+
+      return indexes
+
+  def __data_generation(self, labels, list_IDs_temp,list_IDs):
+      'Generates data of batch_size samples' # X : (n_samples, v_size, v_size, v_size, n_channels)
+      # Initialization
+      # Add 1 at the end, because de depth of the chanel is 1
+      X = np.empty((self.batch_size, self.dim_z, self.dim_x, self.dim_y,1))
+      y = np.empty((self.batch_size), dtype = int)
+
+      # Generate data
+      for i, ID in enumerate(list_IDs_temp):
+          # ID is i in patient(i)
+          patients = os.listdir(list_IDs)
+          patient_path = os.path.join(list_IDs,patients[ID])
+          im = full_process(patient_path,self.dim_z,self.dim_x,self.dim_y)
+          X[i, :, :, :,0] = im
+
+          # Store class
+          y[i] = labels[i]
+
+      return X, y
+
---
+++ Python/Scripts/pre_processing.py
@@ -0,0 +1,137 @@
+import os
+import dicom
+import pylab
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+
+from scipy import ndimage as ndi
+from skimage.filters import roberts, sobel
+from skimage.segmentation import clear_border
+from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
+
+
+
+
+
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+        
+    return slices
+
+
+def PixelData_to_HU(slices):
+    #Stack de nos slices dans une tenseur 3D
+    image = np.stack([s.pixel_array for s in slices])
+
+
+    # Les pixels "aberrant (- 2000) sont associes a l'air, qui vaut -1000HU, le rescale valant en general -1024 on fait :
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 24
+    
+    # Convertion en HU
+    for slice_number in range(len(slices)):
+        
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+        
+        image[slice_number] = slope * image[slice_number] + intercept
+            
+    
+    return np.array(image)
+
+
+
+def get_segmented_lungs(im,seuil=-500):
+    
+    #Seuillage
+    binary = im < seuil
+    
+    #Gestion du bord
+    binary = clear_border(binary)
+    
+    #Closing
+    selem = disk(10)
+    binary = binary_closing(binary, selem)
+    
+    #Filling
+    edges = roberts(binary)
+    binary= ndi.binary_fill_holes(edges)
+    
+    #Masque
+    get_high_vals = binary == 0
+    im[get_high_vals] = 0
+    
+    return im
+
+def normalize(image):
+	MIN_BOUND = -1024
+	MAX_BOUND = 400
+	image = image.astype(np.float16)
+	image = image / (MAX_BOUND - MIN_BOUND)
+	return image
+
+
+
+
+def center(image):
+    #Trop complique de calculer sur toutes les donnees la moyenne, 0.25 est la moyenne empirique, ca sera suffisant
+    #POUR L INSTANT, ON NE SE SERT PAS DU CENTERING (cf notebook), ON REVIENDRA PEUT ETRE ICI PLUS TARD
+    im_mean = 0
+    image = image-im_mean
+    return image
+
+def resample(input_image, scan, new_spacing=[1,1,1]):
+    
+    #L espacement tel qu il est donne
+    prior_spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+    #Le facteur de dilatation pour avoir notre nouvel espacement
+    resize_factor = prior_spacing / new_spacing
+    #Nouvel dimension de l image totale (cad toutes les slices)
+    new_real_shape = input_image.shape * resize_factor
+    #Mieux avec quelque chose d'entier
+    new_shape = np.round(new_real_shape)
+    
+    real_resize_factor = new_shape / input_image.shape
+    new_spacing = prior_spacing / real_resize_factor    
+    #La fonction qui gere tout : scipy.ndimage
+    output_image = scipy.ndimage.interpolation.zoom(input_image, real_resize_factor, mode='nearest')
+    return output_image, new_spacing
+
+
+def zero_padding(im,max_z=400,max_x=450,max_y=450):
+    #Les dimensions sont prises empiriquement, au vu des shape montrees precedemment 
+    borders = (max_z,max_x,max_y)
+    fill_total = [[0,0],[0,0],[0,0]]
+    for i in range(3):
+        current_shape = im.shape[i]
+        if (current_shape < borders[i]):
+            fill = borders[i] - current_shape
+            fill_left = fill/2
+            fill_right = fill/2 + fill % 2
+            fill_total[i] = (fill_left,fill_right)
+    im = np.pad(im,((fill_total[0][0],fill_total[0][1]),(fill_total[1][0],fill_total[1][1]),(fill_total[2][0],fill_total[2][1])),'constant',constant_values=0)
+    #Si jamais notre image depasse les dimensions du zero padding, on va la cut de base
+    im = im[0:max_z,0:max_x,0:max_y]
+    return im
+
+#Fait tout le pre-processing sur une image
+def full_process(patient_path,max_z=400,max_x=450,max_y=450):
+	patient = load_scan(patient_path)
+	image = PixelData_to_HU(patient)
+	image,space = resample(image,patient)
+	image = np.stack([get_segmented_lungs(s) for s in image])
+	image =  np.stack([center(normalize(s)) for s in image])
+	image = zero_padding(image,max_z,max_x,max_y)
+	return image
+
